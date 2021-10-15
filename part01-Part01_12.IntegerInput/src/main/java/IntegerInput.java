9f3894ff45d7da26ddc28b6eecf4c6ca8b735a43
Siddharth Manthan <siddharth.manthan@gmail.com>
2021-10-15 22:03:00 +0530

    soutions: Add solutions from "Part01_11-VariousVariables" to "Part01_14-BooleanInput"
    Signed-off-by: Siddharth Manthan <siddharth.manthan@gmail.com>

--- part01-Part01_12.IntegerInput/src/main/java/IntegerInput.java
+++ part01-Part01_12.IntegerInput/src/main/java/IntegerInput.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class IntegerInput {
 
     public static void main(String[] args) {
+        System.out.println("Give a number:");
         Scanner scanner = new Scanner(System.in);
-
-        // write your program here
-
+        int number=Integer.valueOf(scanner.nextLine());
+        System.out.println("You gave the number "+number);
     }
 }
