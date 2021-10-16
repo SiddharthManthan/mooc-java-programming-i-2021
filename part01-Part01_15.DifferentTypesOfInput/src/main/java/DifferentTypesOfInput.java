ca4357287d4fc62ebe7be793ac75539d4884bb0e
Siddharth Manthan <siddharth.manthan@gmail.com>
2021-10-16 10:19:32 +0530

    soutions: Add solutions from "Part01_15-DifferentTypesOfInput" to "Part01_30-LargerThanOrEqualTo"
    Signed-off-by: Siddharth Manthan <siddharth.manthan@gmail.com>

--- part01-Part01_15.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
+++ part01-Part01_15.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
@@ -5,8 +5,17 @@ public class DifferentTypesOfInput {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // Write your program here
-
+        System.out.println("Give a string:");
+        String value_string=scan.nextLine();
+        System.out.println("Give an integer:");
+        int value_integer=Integer.valueOf(scan.nextLine());
+        System.out.println("Give a double:");
+        double value_double=Double.valueOf(scan.nextLine());
+        System.out.println("Give a boolean:");
+        boolean value_boolean=Boolean.valueOf(scan.nextLine());
+        System.out.println("You gave the string "+value_string);
+        System.out.println("You gave the integer "+value_integer);
+        System.out.println("You gave the double "+value_double);
+        System.out.println("You gave the boolean "+value_boolean);
     }
 }
--- part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
+++ part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -5,6 +5,19 @@ public class LargerThanOrEqualTo {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int greaterNumber=0;
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scan.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scan.nextLine());
+        if (first>second) {
+            greaterNumber=first;
+        } else if (first<second) {
+            greaterNumber=second;
+        } else if (first==second) {
+            System.out.println("The numbers are equal!");
+            return;
+        }
+        System.out.println("Greater number is: "+greaterNumber);
     }
 }
